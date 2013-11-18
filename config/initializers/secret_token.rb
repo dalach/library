ddf9ee2e937caf47dafa56cdbb272a933036f158
dalach <wikadalach@gmail.com>
2013-11-18 16:01:01 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Biblioteka::Application.config.secret_key_base = '6e2369d43b3e2c2f008ec5627d3aab842a1e3d96fc423634e70519149ca83d62df29eda9c40f1f530a2186b49509508b15081b311df4589a0dbe1a279ef7856b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Biblioteka::Application.config.session_store :cookie_store, key: '_biblioteka_session'
